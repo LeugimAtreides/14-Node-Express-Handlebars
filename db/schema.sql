cc863e5e3f56ce423d2f33027139f3cace4ca5b2
LeugimAtreides <m.villarreal789@hotmail.com>
2019-07-09 14:27:12 -0400

    Setup

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE IF NOT EXISTS burger_db;
+
+USE burger_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+    id int not null auto_increment,
+    burger_name varchar(250) not null,
+    devoured BOOL DEFAULT false,
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+
+use burger_db
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter-pounder with Cheese', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Baconator', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Conflicted Burger', FALSE);
