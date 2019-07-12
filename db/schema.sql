05d02ce2582d1bd0c59036faa7a2885f534c81a6
LeugimAtreides <m.villarreal789@hotmail.com>
2019-07-11 21:23:54 -0400

    Adding JawsDB

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,20 @@ CREATE TABLE burgers (
     devoured BOOL DEFAULT false,
     primary key (id)    
 );
+
+
+use burger_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Burger', true);
+
+INSERT INTO burgers (burger_name)  VALUES ('Double Quarter-pounder with Cheese');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Baconator', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Conflicted Burger', true);
+
+select * FROM burgers;
