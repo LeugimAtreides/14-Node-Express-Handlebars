180d22f640f5ff36a7bc987dacc3639716fb85ec
LeugimAtreides <m.villarreal789@hotmail.com>
2019-07-12 11:01:13 -0400

    Heroku changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,3 @@
-DROP DATABASE IF EXISTS burger_db;
-
-CREATE DATABASE IF NOT EXISTS burger_db;
-
-USE burger_db;
-
-DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
     id int not null auto_increment,
